e8621a72f6420785dcbd94e7dfb47ba0dd6c0822
Ricou12 <dwwm2019@gmail.com>
2020-01-08 22:39:48 +0100

    methode map et reduce

--- main.js
+++ main.js
@@ -117,24 +117,6 @@ const showCityFetch = async function (city) {
 }
 showCityFetch("marseille");
 
-// AUTRE METHODE
-// Si je clique sur le nom de la ville j'affiche l'input pour la recherche
-// let ville = document.querySelector('.info-meteo__search__link');
-// ville.addEventListener('click', function () {
-//     showInput();
-//     let myInput = document.querySelector(".info-meteo__search__input");
-//     myInput.addEventListener('change', onInputChanged);
-
-//     function onInputChanged(e) {
-//         city = myInput.value;
-//         showCityFetch(city);
-//         // Remove the event listener
-//         myInput.removeEventListener('input', onInputChanged);
-
-//         // Remove the input
-//         hideInput();
-//     }
-// });
 
 /* -----------------------------------------------------------------------------
                         AFFICHAGE DES INFO METEO
@@ -158,13 +140,14 @@ function update(response) {
     document.querySelector('.info-meteo__temp-medium').textContent = response.current_condition.tmp + "°C";
     document.querySelector('.info-meteo__temp-hight').textContent = response.fcst_day_0.tmax + "°C";
     document.querySelector('.info-meteo__date').textContent = response.fcst_day_0.day_long + " " + response.fcst_day_0.date;
-    generate_prevision(response);
+    document.querySelector('.prevision').innerHTML = generate_prevision(response);
 }
 
 function generate_prevision(response) {
+    /*
+    METHODE N°1
     let properties = [response.fcst_day_0, response.fcst_day_1, response.fcst_day_2, response.fcst_day_3, response.fcst_day_4];
     let html = "";
-    // let test3 = response["fcst_day_" + 0];
     for (let property of properties) {
         html += `<aside class="col-3">
             <h4>${property.day_short}</h4>
@@ -175,5 +158,76 @@ function generate_prevision(response) {
             <aside class="col-3 text-right" style="white-space: nowrap;"><h4>${property.tmin} °C</h4></aside>
             <aside class="col-3 text-right" style="white-space: nowrap;"><h4>${property.tmax} °C</h4></aside>`
     }
-    document.querySelector('.prevision').innerHTML = html
+    return html
+    */
+   
+    /*
+    METHODE N°2 : MAP RENVOIE UN TABLEAU AUGMENTE A CHAQUE OCCURRENCE DU TABLEAU PASSE DS LA FONCTION MAP
+    return [1, 2, 3, 4].map(days => {
+        return `<aside class="col-3">
+                    <h4>${response["fcst_day_" + days].day_short}</h4>
+                </aside>
+                <aside class="col-3 text-right">
+                    <img src="${response["fcst_day_" + days].icon}" alt="">
+                </aside>
+                <aside class="col-3 text-right" style="white-space: nowrap;">
+                    <h4>${response["fcst_day_" + days].tmin} °C</h4>
+                </aside>
+                <aside class="col-3 text-right" style="white-space: nowrap;">
+                    <h4>${response["fcst_day_" + days].tmax} °C</h4>
+                </aside>` 
+    }).join('');  
+    */
+
+    /* METHODE N°3 : MAP
+    return [1, 2, 3, 4].map(days => response["fcst_day_" + days]).map(day => {
+        return `<aside class="col-3">
+                    <h4>${day.day_short}</h4>
+                </aside>
+                <aside class="col-3 text-right">
+                    <img src="${day.icon}" alt="">
+                </aside>
+                <aside class="col-3 text-right" style="white-space: nowrap;">
+                    <h4>${day.tmin} °C</h4>
+                </aside>
+                <aside class="col-3 text-right" style="white-space: nowrap;">
+                    <h4>${day.tmax} °C</h4>
+                </aside>` 
+    }).join('');
+    */
+
+    // METHODE N°4 : REDUCE RENVOIE UNE SEUL VALEUR A PARTIR DE TOUTE LES OCCURRENCES
+    return [1, 2, 3, 4].reduce((accumlator,currentValue) => {
+        const day =  response["fcst_day_" + currentValue];
+       
+        return accumlator + `<aside class="col-3">
+                    <h4>${day.day_short}</h4>
+                </aside>
+                <aside class="col-3 text-right">
+                    <img src="${day.icon}" alt="">
+                </aside>
+                <aside class="col-3 text-right" style="white-space: nowrap;">
+                    <h4>${day.tmin} °C</h4>
+                </aside>
+                <aside class="col-3 text-right" style="white-space: nowrap;">
+                    <h4>${day.tmax} °C</h4>
+                </aside>` 
+    },"");
+
+    /* METHODE N°5 : REDUCE RACCOURCI
+    return [1, 2, 3, 4].reduce((accumlator, currentValue) => accumlator + 
+               `<aside class="col-3">
+                    <h4>${response["fcst_day_" + currentValue].day_short}</h4>
+                </aside>
+                <aside class="col-3 text-right">
+                    <img src="${response["fcst_day_" + currentValue].icon}" alt="">
+                </aside>
+                <aside class="col-3 text-right" style="white-space: nowrap;">
+                    <h4>${response["fcst_day_" + currentValue].tmin} °C</h4>
+                </aside>
+                <aside class="col-3 text-right" style="white-space: nowrap;">
+                    <h4>${response["fcst_day_" + currentValue].tmax} °C</h4>
+                </aside>`
+   , "");
+   */
 }
